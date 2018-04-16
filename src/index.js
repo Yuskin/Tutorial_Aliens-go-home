a39b07f3ff7aa74071a6c83da6e7f252ce2001fe
Yus <Yuskinta@gmail.com>
2018-04-16 13:39:26 +0300

    tutorial completed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './index.css';
 import Game from './containers/Game';
-import reducer from './reducers/src/index.js';
+import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
 /* eslint-disable no-underscore-dangle */
--- src/reducers/src/index.js
+++
@@ -1,9 +0,0 @@
-const initialState = {
-	message: 'React и Redux легко интегрируются, не так ли?',
-};
-
-function reducer(state = initialState) {
-	return state;
-}
-
-export default reducer;
